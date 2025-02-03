8daa9ebb4ff02c19f43b02ce95dd76a27ba8254b
Vishal Kamble <vishalkamble0088@gmail.com>
2025-02-03 17:28:12 +0530

    basic program

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", {id:"parent"}, [
+    React.createElement("div", {id:"child"}, [
+        React.createElement("h1", {}, "This is a h1 tag"),
+        React.createElement("h2", {}, "This is a h2 tag")
+    ]),
+    React.createElement("div", {id:"child2"}, [
+        React.createElement("h1", {}, "This is a h1 tag"),
+        React.createElement("h2", {}, "This is a h2 tag")
+    ])
+] );
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
